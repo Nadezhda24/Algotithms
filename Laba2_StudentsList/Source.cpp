a60c57b84724972bda08be4264b028492ce39ae8
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2020-09-20 15:09:23 +0300

    Laba2

---
+++ Laba2_StudentsList/Source.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+struct Student {
+	char surname[50];
+	int exam1;
+	int exam2;
+	int exam3;
+	bool certificate;
+	char city[50];
+	bool hostel;
+};
+
+
+
+int main() {
+
+
+	system("pause");
+	return 0;
+}
